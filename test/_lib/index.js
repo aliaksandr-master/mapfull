0e28736304dfa511e7621dbe9e064f8addbb79f0
ALiaksandr Pasynkau <alxe.master@gmail.com>
2015-06-27 00:03:24 +0300

    add lib structure

---
+++ test/_lib/index.js
@@ -0,0 +1,3 @@
+"use strict";
+
+module.exports = process.env.NODE_LIB_ROUTEFULL_COVERAGE ? require('./../../lib-cov') : require('./../../lib');
---
+++ test/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var _ = require('lodash');
+var routefull = require('./_lib');
+
+exports['test'] = function (test) {
+	test.ok(true);
+
+	test.done();
+};
